5593cd31624b0775b1be644e6b68bc8fd622cf2a
trevor <me@trevorbehnke.com>
2021-11-03 18:26:40 -0700

    feat: :monocle_face: through 50

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import TripList from "./components/TripList";
 
 function App() {
-  return <div className="App">Yo!</div>;
+  return (
+    <div className="App">
+      <TripList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TripList.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+import "./TripList.css";
+
+export default function TripList() {
+  const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setTrips(json));
+  }, [url]);
+
+  return (
+    <div className="trip-list">
+      <h2>Trip List</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=Europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
+    </div>
+  );
+}
